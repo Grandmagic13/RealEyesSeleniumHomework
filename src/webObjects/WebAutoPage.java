e8f3caae262f013649b8ef8a7bb5b32b457b93cb
Grandmagic13 <jabencefear@hotmail.com>
2018-02-11 17:40:56 +0100

    First scenario automated and running locally with gecko

--- src/webObjects/WebAutoPage.java
+++ src/webObjects/WebAutoPage.java
@@ -1,11 +1,88 @@
 package webObjects;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotSelectableException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class WebAutoPage extends PageObject {
 
+	@FindBy(css = "img[alt=login-page]")
+	private WebElement downArrow;
+
+	@FindBy(tagName = "button")
+	private WebElement loginButton;
+
+	@FindBy(css = "input[type=text]")
+	private WebElement enterUserName;
+
+	@FindBy(css = "input[type=password]")
+	private WebElement enterPassword;
+
 	public WebAutoPage(WebDriver driver) {
 		super(driver);
+	}
+
+	public boolean isInitialized() {
+		return downArrow.isDisplayed();
+	}
+
+	public void enterUserName(String userName) {
+		enterInputTo(userName, enterUserName);
+	}
+
+	public void enterPassword(String password) {
+		enterInputTo(password, enterPassword);
+	}
+
+	public void enterInputTo(String inputText, WebElement inputField) {
+		inputField.clear();
+		inputField.sendKeys(inputText);
+	}
+
+	public void login() {
+		if (loginButton.getText().equals("Login")) {
+			loginButton.click();
+		} else {
+			throw new ElementNotSelectableException("login button can't be found!");
+		}
+	}
+
+	public void divideSevenByZero() {
+		WebElement seven = getButtonByText("7");
+		WebElement zero = getButtonByText("0");
+		WebElement equals = getButtonByText("=");
+		Select operationSelector = new Select(driver.findElement(By.tagName("select")));
+
+		seven.click();
+		operationSelector.selectByValue("/");
+		zero.click();
+		equals.click();
+	}
+
+	private WebElement getButtonByText(String text) {
+		for (WebElement button : driver.findElements(By.tagName("button"))) {
+			if (button.getText().equals(text)) {
+				return button;
+			}
+		}
+		String exceptionMessage = String.format("'%s' button can't be found!", text);
+		throw new ElementNotSelectableException(exceptionMessage);
+	}
+
+	public String getAlertText() {
+		return getAlertPopup().getText();
+	}
+
+	public void closeAlert() {
+		getAlertPopup().accept();
+	}
+
+	private Alert getAlertPopup() {
+		return driver.switchTo().alert();
 	}
 
 }
--- test/webTests/HomeworkTests.java
+++ test/webTests/HomeworkTests.java
@@ -1,18 +1,32 @@
 package webTests;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+
+import webObjects.WebAutoPage;
 
 public class HomeworkTests extends SeleniumTest {
 
-	// Narrative: #1 Dividing by zero should pop an alert
 	@Test
-	public void dividingByZeroShouldPopAnAlert() {
+	public void dividingByZeroShouldPopAnAlert() throws InterruptedException {
+		// Narrative: #1 Dividing by zero should pop an alert
+		// Steps:
 		// 1. Open https://webautomationhw.realeyesit.com/
+		driver.get("https://webautomationhw.realeyesit.com/");
+		WebAutoPage webAutoPage = new WebAutoPage(driver);
+		assertTrue(webAutoPage.isInitialized());
 		// 2. Log in using the following credentials: testuser / testpassword
+		webAutoPage.enterUserName("testuser");
+		webAutoPage.enterPassword("testpassword");
 		// 3. Press login button
+		webAutoPage.login();
 		// 4. Divide an arbitrary number with 0
+		webAutoPage.divideSevenByZero();
 		// 5. Check if the popup showed and displaying the message
+		assertFalse(webAutoPage.getAlertText().equals(""));
 		// 6. Close the popup
+		webAutoPage.closeAlert();
 	}
 
 }
