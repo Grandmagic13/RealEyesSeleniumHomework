a48203a6a898c201c2c526c721f637d1a20b8734
Grandmagic13 <jabencefear@hotmail.com>
2018-02-11 20:21:02 +0100

    Created remotely runnable (Browserstack) version.

--- test/webTests/SeleniumTest.java
+++ test/webTests/SeleniumTest.java
@@ -1,29 +1,38 @@
 package webTests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class SeleniumTest {
 
+	private static final String USERNAME = System.getProperty("username");
+	private static final String AUTOMATE_KEY = System.getProperty("automate_key");
+	private static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+	private static final HashMap<String, String> defaultValues = initDefaultValues();
 	protected static WebDriver driver;
 
 	@BeforeClass
-	public static void setUp() {
-		String browserType = System.getProperty("browsertype");
-		if (browserType.equals("firefox")) {
-			driver = new FirefoxDriver();
-		} else if (browserType.equals("chrome")) {
-			driver = new ChromeDriver();
-		} else {
-			throw new RuntimeException("Unknown browsertype!");
-		}
+	public static void setUp() throws MalformedURLException {
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("browser", getValueForCapability("browser"));
+		caps.setCapability("browser_version", getValueForCapability("browser_version"));
+		caps.setCapability("os", getValueForCapability("os"));
+		caps.setCapability("os_version", getValueForCapability("os_version"));
+		caps.setCapability("resolution", getValueForCapability("resolution"));
+
+		driver = new RemoteWebDriver(new URL(URL), caps);
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 	}
 
 	@After
@@ -33,6 +42,20 @@ public class SeleniumTest {
 
 	@AfterClass
 	public static void tearDown() {
-		driver.close();
+		driver.quit();
+	}
+
+	private static String getValueForCapability(String capability) {
+		return System.getProperty(capability) != null ? System.getProperty(capability) : defaultValues.get(capability);
+	}
+
+	private static HashMap<String, String> initDefaultValues() {
+		HashMap<String, String> defaults = new HashMap<>();
+		defaults.put("browser", "Chrome");
+		defaults.put("browser_version", "64.0");
+		defaults.put("os", "Windows");
+		defaults.put("os_version", "8.1");
+		defaults.put("resolution", "1280x1024");
+		return defaults;
 	}
 }
